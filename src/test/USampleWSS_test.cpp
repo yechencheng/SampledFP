b52f7bb575cdb2e259e03f9b159778a36c604936
cc <yechencheng@gmail.com>
2017-10-28 03:18:47 +0800

    nothing

--- src/test/USampleWSS_test.cpp
+++ src/test/USampleWSS_test.cpp
@@ -4,7 +4,6 @@
 #include <cstdlib>
 #include <queue>
 
-#include "sampler.h"
 #include "type.h"
 
 using namespace std;
@@ -16,7 +15,9 @@ int64_t current_pos = 0;
 queue<Access> q;
 
 void output_ws(int64_t wss, int64_t num=1){
-    cout << wss << " " << num << endl;
+    //cout << wss << " " << num << endl;
+    while(num--)
+        cout << wss << " ";
 }
 
 void update_wss(AddrInt addr, int64_t pos, int ws){
@@ -68,22 +69,40 @@ void update_wss(AddrInt addr, int64_t pos, int ws){
     }
 }
 
-int main(int argc, char** argv){
-    if(argc < 4){
-        cerr << "usage : command trace_file window_length strip" << endl;
-        return 1;
+unordered_map<AddrInt, int64_t> px;
+queue<AddrInt> qx;
+int wssx = 0;
+void burst(AddrInt addr, int64_t pos, int ws){
+    if(px.find(addr) == px.end() || px[addr] <= pos-ws)
+        wssx++;
+    px[addr] = pos;
+    qx.push(addr);
+    if(qx.size() < ws) return;
+
+    if(px.find(0) != px.end() && px[0] > pos-ws)
+        output_ws(wssx-1, 1);
+    else
+        output_ws(wssx, 1);
+    if(px[qx.front()] == pos-ws+1)
+        wssx--;
+    qx.pop();
+}
+
+int main(){
+    AddrInt a[] = {1,2,3,3,2,1,1,2,3};
+    int64_t pos[] = {0,2,3,5,6,7,8,9,14};
+    int len = sizeof(a)/sizeof(AddrInt);
+    int ws = 5;
+    for(int i = 0; i < len; i++){
+        update_wss(a[i], pos[i], ws);
     }
-
-    string fname = string(argv[1]);
-    int ws = atoi(argv[2]);
-    int strip = atoi(argv[3]);
-
-    UniformSampler usamp(fname, strip);
-    AddrInt addr;
-
-    int64_t pos;
-    while((pos = usamp.next(addr)) != -1){
-        update_wss(addr, pos, ws);
+    cout << endl;
+    
+    for(int i = 0, p = 0; i < len; i++){
+        while(p < pos[i])
+            burst(0, p++, ws);
+        burst(a[i], p++, ws);
     }
+    cout << endl;
     return 0;
 }
