099c5cfa4db52b9ec7ced02819855b10a4b360f3
ccalpha <yechencheng@gmail.com>
2017-11-07 03:36:47 +0800

    nothing

--- src/plot/histWSS.cpp
+++ src/plot/histWSS.cpp
@@ -11,13 +11,13 @@ using namespace boost::program_options;
 
 int main(int argc, char** argv){
     string fname;
-    int64_t nsample = -1;
+    int64_t nwss = -1;
 
     options_description desc{"Options"};
     desc.add_options()
         ("help,h", "This message")
         ("input,i", value<string>(&fname)->required(), "input trace file")
-        ("nsample,n", value<int64_t>(&nsample), "number of sampled")
+        ("nsample,n", value<int64_t>(&nwss), "number of sampled")
     ;
     variables_map vm;
     store(parse_command_line(argc, argv, desc), vm);
@@ -42,9 +42,10 @@ int main(int argc, char** argv){
         cw.update(wss, num, nsampled);
     }
     //cout << cw.cnt.size() << endl;
-    if(nsample != -1)
-        cout << cw.cnt[nsample].first << "\t" << cw.cnt[nsample].second << endl;
-    return 0;
+    if(nwss != -1){
+        cout << cw.cnt[nwss].first << "\t" << cw.cnt[nwss].second << endl;
+        return 0;
+    }
     for(auto &i : cw.cnt){
         cout << i.first << "\t" << i.second.first << "\t" << i.second.second << endl;
     }
